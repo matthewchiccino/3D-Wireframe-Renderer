b4aa026aca03d3914423416283e38631e2c87760
matthewchiccino <matthew.chiccino@outlook.com>
2024-12-18 11:09:49 -0500

    file organization

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib.widgets import Slider, RadioButtons
 from matplotlib.collections import LineCollection
 from functionality import *
+from wireframes import *
 
 #############################
 # MATPLOTLIB CONFIGURATIONS #
@@ -31,62 +32,6 @@ axes['main'].set_yticks([])
 axes['main'].set_title("3D Graphics Demo")
 axes['shape'].set_title("Shapes")
 axes['log'].axis('off')
-
-###############
-# WIRE FRAMES #
-###############
-
-cube = [[(-1, -1, -1), (1, -1, -1)],
-        [(1, -1, -1), (1, 1, -1)],
-        [(1, 1, -1), (-1, 1, -1)],
-        [(-1, 1, -1), (-1, -1, -1)],
-        [(-1, -1, -1), (-1, -1, 1)],
-        [(1, -1, -1), (1, -1, 1)],
-        [(1, 1, -1), (1, 1, 1)],
-        [(-1, 1, -1), (-1, 1, 1)],
-        [(-1, -1, 1), (1, -1, 1)],
-        [(1, -1, 1), (1, 1, 1)],
-        [(1, 1, 1), (-1, 1, 1)],
-        [(-1, 1, 1), (-1, -1, 1)]]
-
-pyramid = [[(-1, -1, -1), (-1, -1, 1)],
-           [(-1, -1, 1), (1, -1, 1)],
-           [(1, -1, 1), (1, -1, -1)],
-           [(1, -1, -1), (-1, -1, -1)],
-           [(-1, -1, -1), (0, 1, 0)],
-           [(-1, -1, 1), (0, 1, 0)],
-           [(1, -1, 1), (0, 1, 0)],
-           [(1, -1, -1), (0, 1, 0)]]
-
-def mk_circle(r, n, z):
-    out = []
-    for i in range(n):
-        out.append(((r * np.cos(2 * np.pi * i / n), r * np.sin(2 * np.pi * i / n), z),
-                    (r * np.cos(2 * np.pi * (i + 1) / n), r * np.sin(2 * np.pi * (i + 1) / n), z)))
-    return out
-
-def mk_wheel_edge(r, n, z1, z2):
-    out = []
-    for i in range(n):
-        out.append(((r * np.cos(2 * np.pi * i / n), r * np.sin(2 * np.pi * i / n), z1),
-                    (r * np.cos(2 * np.pi * i / n), r * np.sin(2 * np.pi * i / n), z2)))
-    return out
-
-def mk_wheel_face(r1, r2, n, z):
-    out = []
-    for i in range(n):
-        out.append(((r1 * np.cos(2 * np.pi * i / n), r1 * np.sin(2 * np.pi * i / n), z),
-                    (r2 * np.cos(2 * np.pi * i / n), r2 * np.sin(2 * np.pi * i / n), z)))
-    return out
-
-wheel = mk_circle(3, 50, 1) + \
-    mk_circle(1, 50, 1) + \
-    mk_circle(3, 50, -1) + \
-    mk_circle(1, 50, -1) + \
-    mk_wheel_edge(3, 50, 1, -1) + \
-    mk_wheel_edge(1, 50, 1, -1) + \
-    mk_wheel_face(1, 3, 50, 1) + \
-    mk_wheel_face(1, 3, 50, -1)
 
 guide_axes = [[(-0.75, 0, 0), (0.75, 0, 0)],
               [(0, -0.75, 0), (0, 0.75, 0)],
